119d491c2aea314a04b1d4597d20f97242fb1990
Ishita Karmakar <115342916+Kat-o30@users.noreply.github.com>
2024-11-07 14:40:23 +0530

    database update first

--- utils/database.js
+++ utils/database.js
@@ -1,26 +1,26 @@
 import mongoose from 'mongoose';
 
-let isConnected = false; //track the connection 
+let isConnected = false; // Track the connection status
 
 export const connectToDB = async () => {
-    mongoose.set('strictQuery', true)
+    // Enable strict mode for query
+    mongoose.set('strictQuery', true);
 
-    if(isConnected) {
+    if (isConnected) {
         console.log('MongoDB is already connected');
         return;
     }
 
     try {
+        // Use MONGODB_URI from environment variables
         await mongoose.connect(process.env.MONGODB_URI, {
             dbName: "share_prompt",
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+        });
 
         isConnected = true;
         console.log("MongoDB connected");
+    } catch (error) {
+        console.error("MongoDB connection error:", error);
+        throw new Error("Failed to connect to MongoDB");
     }
-    catch(error) {
-        console.log(error);
-    }
-}
+};
